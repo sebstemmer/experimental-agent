dc51be31143ce710705f4a2586ecd1215283376f
Sebastian Stemmer <stemmer.main@gmail.com>
2026-08-31 14:33:39 +0200

    main

--- src/morning_briefing/job.py
+++ src/morning_briefing/job.py
@@ -7,8 +7,11 @@ from telegram.ext import ContextTypes, JobQueue
 
 from channels.handle_message import handle_message
 
-BRIEFING_TIME = time(14, 32, tzinfo=ZoneInfo("Europe/Berlin"))
-BRIEFING_PROMPT = "List my open todos."
+BRIEFING_TIME = time(14, 35, tzinfo=ZoneInfo("Europe/Berlin"))
+BRIEFING_PROMPT = (
+    "Start with a short greeting like 'Good morning Sebastian', "
+    "then list my open todos."
+)
 
 logger = logging.getLogger(__name__)
 
